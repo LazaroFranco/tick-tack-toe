529f5a71c5af31f0b604e469d6bd0b127454046b
Lazaro Franco <lazaro300x@icloud.com>
2018-11-12 14:07:57 -0500

    t-t-t game

---
+++ game.js
@@ -0,0 +1,10 @@
+const mySymbol = () => {
+  let x = player1;
+  let o = player2;
+
+
+  let button = document.querySelector("button");
+  button.addEventListener("click", () => {
+    console.log("x");
+  });
+};
