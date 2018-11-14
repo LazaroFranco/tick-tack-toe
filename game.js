2ba44437eca89127aa420954174a057f68d488b0
Lazaro Franco <lazaro300x@icloud.com>
2018-11-14 12:34:51 -0500

    coded!

--- game.js
+++ game.js
@@ -1,10 +1,46 @@
-const mySymbol = () => {
-  let x = player1;
-  let o = player2;
+let player = 'x' || 'o';
 
-
-  let button = document.querySelector("button");
-  button.addEventListener("click", () => {
-    console.log("x");
-  });
+const resetGrid = () => {
+  grid = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ];
 };
+const insertSymbol = (symbol, x, y) => {
+  grid[y][x] = symbol;
+
+  drawGrid();
+};
+
+const clearChildren = (element) => {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+};
+
+const drawGrid = () => {
+  const tableEl = document.querySelector('table');
+  clearChildren(tableEl);
+
+  for (let row of grid) {
+    let rowEl = document.createElement('tr');
+
+    for (let column of row) {
+      let cellEl = document.createElement('td');
+
+      let symbol = column;
+      if (column === null) {
+        symbol = ' ';
+      }
+      let cellTxt = document.createTextNode(symbol);
+      cellEl.appendChild(cellTxt);
+      rowEl.appendChild(cellEl);
+    }
+    tableEl.appendChild(rowEl);
+  }
+};
+resetGrid();
+insertSymbol('x', 2, 0);
+insertSymbol('o', 1, 1);
+drawGrid();
